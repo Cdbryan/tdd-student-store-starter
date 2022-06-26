3aa3a4e3257a57476eb89bcc7c0741e7e110d2a2
Christian Bryan <christian.bryan@christi-ltm3ydr.internal.salesforce.com>
2022-06-25 23:00:50 -0700

    improved homepage layout

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -9,10 +9,18 @@ export default function ProductCard({ product, productID, showDescription, handl
       <Link to={`/products/${productID}`} className="media">
         <img className="productImage" src={product.image}></img>
       </Link>
-      <h1 className="productName"> {product.name} </h1>
-      <h1 className="price"> ${product.price} </h1>
-      <button className="add" onClick={handleAddItemToCart(productID)}> + </button>
-      <button className="remove" onClick={handleRemoveItemFromCart(productID)}> - </button>
+      <div className="productInfo">
+        <h1 className="productName"> {product.name} </h1>
+        <h1 className="price"> ${product.price} </h1>
+        <span className="span">
+          <button className="add" onClick={handleAddItemToCart(productID)}> 
+            <i class="material-icons">add</i>
+          </button>
+          <button className="remove" onClick={handleRemoveItemFromCart(productID)}> 
+            <i class="material-icons">remove</i>
+          </button>
+        </span>
+      </div>
       {/* if(showDescription)
         {
             // <h1 className="description"> ${product.description} </h1>
