28ec789ab881b5f839098b8f3bde5c6cb6d9f379
Christian Bryan <christian.bryan@christi-ltm3ydr.internal.salesforce.com>
2022-07-05 15:48:59 -0700

    post request working

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const Store = require("./models/store")
-const router = express.Router();
+const {storage} = require("./data/storage")
+// const router = express.Router();
 
 var cors = require("cors");
 app.use(cors());
@@ -14,7 +15,7 @@ morgan("tiny");
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.use("/", router);
+// app.use("/", router);
 
 
 app.get("/store", (req, res) => {
@@ -32,12 +33,21 @@ app.get("/", (req, res) => {
   res.status(200).json({ ping: "pong" });
 });
 
-router.post("/", (req,res) => {
-  const user = {
-    name: req.body.name,
-    email: req.body.email
+app.post("/store", (req,res) => {
+  try{
+    const user = {
+      name: req.body.name,
+      email: req.body.email
+    } 
+    // var purchases = storage.get("purchases").value()
+    storage.get("purchases").push(user).write()
+    // console.log("purchases array", purchases)
+    res.status(201).json({user})
   }
-  res.status(201).json({user})
+  catch(e)
+  {
+    console.log(e)
+  }
 })
 
 module.exports = app;
